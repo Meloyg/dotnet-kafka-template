7a70700fed83f2fdfa41474ff7cda53fc6ad6ef9
Melog <melo.guan@zip.co>
2025-09-29 10:26:48 +1300

    update 6 files

--- src/DotnetKafka.Order/Models/Product.cs
+++ src/DotnetKafka.Order/Models/Product.cs
@@ -2,5 +2,12 @@ namespace DotnetKafka.Order.Models;
 
 public class Product
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+
+    public override string ToString()
+    {
+        return $"ProductName:{Name} ,Price :{Price}";
+    }
 }
--- src/DotnetKafka.Order/Services/KafkaConsumerHandler.cs
+++ src/DotnetKafka.Order/Services/KafkaConsumerHandler.cs
@@ -1,6 +1,54 @@
+using System.Text.Json;
+using Confluent.Kafka;
+using DotnetKafka.Order.Models;
+
 namespace DotnetKafka.Order.Services;
 
-public class KafkaConsumerHandler
+public class KafkaConsumerHandler: BackgroundService
 {
     
+    private readonly string topicName = "productTopic";
+
+    
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // customize you configuration
+        ConsumerConfig _consumerConfig = new ConsumerConfig
+        {
+            BootstrapServers = "kafka:9092",
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            ClientId = "Orders",
+            GroupId = "my-group",
+        };
+        
+        using (var consumer = new ConsumerBuilder<Ignore, string>(_consumerConfig)
+                   .SetErrorHandler((_, ex) => Console.WriteLine(ex.Reason))
+                   .Build())
+        {
+            // consumer now listen to that topic if any messages send by producer it will be consumed here 
+            consumer.Subscribe(topicName);
+
+            try
+            {
+                while (true)
+                {
+                    var consumerResult = consumer.Consume();
+                    var product = JsonSerializer.Deserialize<Product>(consumerResult.Message.Value);
+                    // i have the product sent from product App ready to be processed
+                    // write you implementation
+                    Console.WriteLine($" Product Has Been received form PorductApp: => {product}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.Message);
+                throw ex;
+            }
+            finally
+            {
+                consumer.Close();
+            }
+        }
+
+    }
 }
